50c07047d6c9878c604be7d3892af03307236c92
Michael <mbg@outlook.com>
2018-02-21 08:06:24 +0000

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -3,7 +3,7 @@ import HomeSection from './HomeSection'
 
 export default ({ projects }) => (
   <div className='f4-ns f5 w-100 pv3 lh-copy'>
-    <div>I make Internet things to help me learn new technologies:</div>
+    <div>I make Internet things:</div>
     <div>
       {projects.map(project => {
         return <HomeSection key={project.id} {...project} />
