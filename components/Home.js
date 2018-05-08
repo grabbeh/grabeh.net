4b018b2a44978c01a103c7c8bd791160dd1cef21
Michael <mbg@outlook.com>
2018-05-08 23:54:16 +0100

    Style

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
 import HomeSection from './HomeSection'
 
 export default ({ projects }) => (
   <div className='mw8 center f4 pa3'>
-    <div className='mb2 f3'>
-      Hi, my name's Michael and I make Internet things:
+    <div className='mb4 mt3 font f3'>
+      <p>
+        Hi, my name's Michael. I work as an in-house lawyer in London. However in my spare time I make Internet things.
+      </p>
+      <p>
+        In part I'm inspired by the interface between the law and programming but also in part because I just enjoy learning new technologies.
+      </p>
+      <p> Here's some of my work:</p>
     </div>
     <div className='flex flex-wrap'>
       {projects.map(project => {
