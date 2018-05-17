1df4ce64f306995d1ee8e5b0c25ec0c1746c63cf
Michael <mbg@outlook.com>
2018-05-17 23:04:41 +0100

    Update

--- components/SidePage.js
+++ components/SidePage.js
@@ -2,27 +2,21 @@ import styled from 'styled-components'
 import { Component, Fragment } from 'react'
 
 class SidePage extends Component {
-  constructor (props) {
-    super(props)
-  }
-
-  componentDidMount () {
-    console.log(this.props)
-  }
   render () {
-    let { project: { imageUrl, projectName } } = this.props
+    let { project: { imageUrl, projectName, longDescription } } = this.props
     return (
       <MenuAnimation showSidePage={this.props.showSidePage}>
-
         {this.props.project &&
           <Fragment>
             <div onClick={this.props.toggle}>
               <i className='fr pb2 fa fa-times fa-lg' />
             </div>
-            <div className='b mb2 font f3'>{projectName}</div>
+            <div className='b bb bw1 b--black-30 pb2 mb3 font f3'>
+              {projectName.toLowerCase()}
+            </div>
             <img src={imageUrl} />
+            <div className='pt2 pb4 f4 lh-copy'>{longDescription}</div>
           </Fragment>}
-
       </MenuAnimation>
     )
   }
@@ -31,7 +25,7 @@ class SidePage extends Component {
 export default SidePage
 
 const MenuAnimation = styled.div.attrs({
-   className: 'overflow-y-scroll h100 height bg-white pa3 w-50 front fixed'
+  className: 'overflow-y-scroll h-100 height bg-white pa3 w-60-l w-80-ns w-100 front fixed'
 })`
 transform: translateX(${props => (props.showSidePage ? '0px' : '-100%')});
 transition: transform .5s ease-in;
