4e0c1d7a10a1b6fc653a961946f341fb0e435703
Michael <mbg@outlook.com>
2018-07-13 12:03:12 +0100

    Update SideBarItem.js

--- src/components/SideBarItem.js
+++ src/components/SideBarItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-export default ({ children, url, item }) => (
-  <div className='pointer fl br3 mt3 mt0-ns bg-light-yellow mr3 ph3 pv2'>
+const SideBarItem ({ children, url, item }) => (
+  <div className='pointer fl mt3 mt0-ns mr3 ph3 pv2'>
     <Link className='underline-hover link dark-gray' to={url}>
       {children}
       {item}
     </Link>
   </div>
 )
+
+export default SideBarItem
