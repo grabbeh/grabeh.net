14a862af2bdd6fa5dfd8b03d52b500e7c42bd6b2
Michael <mbg@outlook.com>
2018-07-13 16:15:23 +0100

    Update Tool.js

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Tool = ({ children }) => (
-  <div className='mt2 br3 ba mr2 fl pv2 ph3'>
+  <div className='mt2 dark-gray bg-black ba mr2 fl pv2 ph3'>
     {children}
   </div>
 )
