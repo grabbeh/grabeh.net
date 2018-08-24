574609846929c65bb27a0f4ef7fc91309665dae2
Michael <mbg@outlook.com>
2018-08-24 13:41:47 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Roboto']
+          families: ['Libre Franklin']
         }
       }
     },
