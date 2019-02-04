67c19edd69da75fc6973f08810dee3cc03b8b8c2
Michael <mbg@outlook.com>
2019-02-04 13:30:12 +0000

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,10 +1,12 @@
 import Box from './Box'
 import Text from './Text'
 
-const Toast = ({message}) => (
+const Toast = (props) => {
+  console.log(props)
+  return (
   <Box p={3} bg='yellow' position='absolute' bottom={10} right={10}>
-    <Text color='black' fontWeight='bold' fontSize={4}>{message}</Text>
-  </Box>
-)
+    <Text color='black' fontWeight='bold' fontSize={4}>{props.message}</Text>
+  </Box>)
+}
 
 export default Toast
