8d6213a03cb908a6f23b961e7f3af4e890f3c6f6
Michael <mbg@outlook.com>
2019-02-04 16:43:17 +0000

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -5,18 +5,20 @@ import Flex from './Flex'
 import MdClose from 'react-icons/lib/md/close'
 
 const Toast = (props) =>  (
-  <Box zIndex={999} p={3} bg='yellow' position='absolute' top={10} right={10}>
-    <Flex justifyContent='flex-start'>
-      <Box width={0.8}>
-    <Text color='black' fontWeight='bold' fontSize={4}>{props.message}</Text>
-  </Box>
+  <Flex flexWrap='wrap'>
+    <Box zIndex={999} p={3} bg='yellow' position='absolute' top={10} right={10}>
+      <Flex justifyContent='flex-start'>
+        <Box width={0.8}>
+           <Text color='black' fontWeight='bold' fontSize={4}>{props.message}</Text>
+        </Box>
    </Flex>
       <Flex justifyContent='flex-end'>
        <Box width={0.1} onClick={props.remove} p={2}>
         <MdClose />
       </Box>
     </Flex>
-  </Box>)
+  </Box>
+</Flex>)
 
 
 export default Toast
