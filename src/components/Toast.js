c15d94a75b2d3df40b3c00ebc70eb5d38eff9b50
Michael <mbg@outlook.com>
2019-02-04 13:55:22 +0000

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Box from './Box'
 import Text from './Text'
+import MdClose from 'react-icons/md/close'
 
-const Toast = (props) => {
-  console.log(props)
-  return (
+const Toast = (props) =>  (
   <Box zIndex={999} p={3} bg='yellow' position='absolute' top={10} right={10}>
+    <Box onClick={props.remove} position='absolute' top={5} right={5}>
+      <MdClose />
+    </Box>
     <Text color='black' fontWeight='bold' fontSize={4}>{props.message}</Text>
   </Box>)
-}
+
 
 export default Toast
