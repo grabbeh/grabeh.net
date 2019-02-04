d85f029eeed672a9c9bc8c5dae081a97ace3069c
Michael <mbg@outlook.com>
2019-02-04 13:27:14 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import Box from '../components/Box'
 import Flex from '../components/Flex'
 import Text from '../components/Text'
